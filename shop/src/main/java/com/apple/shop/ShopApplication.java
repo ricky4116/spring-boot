174feac85e9504d9d283f538d958f71aab3c73da
ricky <71623014+ricky4116@users.noreply.github.com>
2026-01-14 10:54:37 +0900

    java 기본

--- shop/src/main/java/com/apple/shop/ShopApplication.java
+++ shop/src/main/java/com/apple/shop/ShopApplication.java
@@ -7,8 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ShopApplication.class, args);
+		var test = new Friend("park");
+		System.out.println(test.name);
 	}
-
+}
+class Friend {
+	String name;
+	int age = 20;
+	Friend(String a){
+		this.name=a;
+	}
 }
