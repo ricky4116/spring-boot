e71d7b1faa1dcb24231119240e54522693afe02b
ricky <71623014+ricky4116@users.noreply.github.com>
2026-01-15 14:54:53 +0900

    detail page

--- shop/src/main/java/com/apple/shop/ItemController.java
+++ shop/src/main/java/com/apple/shop/ItemController.java
@@ -3,10 +3,14 @@ package com.apple.shop;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,16 +23,33 @@ public class ItemController {
     String list(Model model){
         List<Item> result = itemRepository.findAll();
         model.addAttribute("items",result);
-        var a = new feature();
-        a.setName("홍길동");
-        a.setAge(20);
-        a.나이설정(-10);
-        a.한살더하기();
-        System.out.println(a.getAge());
-
-        var b= new Item();
-        b.setTitle("hello world");
-        System.out.println(b.getTitle());
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(String title, Integer price) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+        Optional<Item> result = itemRepository.findById(id);
+
+        if (result.isPresent()) {
+            model.addAttribute("data", result.get());
+            return "detail.html";
+        } else {
+            return "redirect:/list";
+        }
     }
 }
